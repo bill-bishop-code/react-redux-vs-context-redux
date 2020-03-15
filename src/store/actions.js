4a9fd8aaba9ac5d99e7fd2ecac9d0c047ab869c3
bbishopski@yahoo.com <bbishopski@yahoo.com>
2020-03-15 10:33:00 -0400

    testing

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,8 +1,9 @@
-export const ADD_PRODUCT_TO_CART = 'ADD_PRODUCT_TO_CART';
-export const REMOVE_PRODUCT_FROM_CART = 'REMOVE_PRODCUT_FROM_CART';
+export const ADD_PRODUCT_TO_CART = "ADD_PRODUCT_TO_CART";
+export const REMOVE_PRODUCT_FROM_CART = "REMOVE_PRODCUT_FROM_CART";
 
 export const addProductToCart = product => {
   return dispatch => {
+    // This is where you could reach out to an API and then dispatch "FETCHING" then "FETCHED"...or use redux_thunk
     setTimeout(() => {
       dispatch({
         type: ADD_PRODUCT_TO_CART,
@@ -13,12 +14,12 @@ export const addProductToCart = product => {
 };
 
 export const removeProductFromCart = productId => {
-    return dispatch => {
-      setTimeout(() => {
-        dispatch({
-          type: REMOVE_PRODUCT_FROM_CART,
-          payload: productId
-        });
-      }, 700);
-    };
+  return dispatch => {
+    setTimeout(() => {
+      dispatch({
+        type: REMOVE_PRODUCT_FROM_CART,
+        payload: productId
+      });
+    }, 700);
   };
+};
